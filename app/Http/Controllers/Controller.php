f113cf110920a5757c8916009961200ae90d6ec0
prismathic <pjjsalam1999@gmail.com>
2020-09-21 19:11:44 +0100

    feat: added general format for sending JSON responses

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,4 +7,13 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     //
+    protected function sendFormattedJsonResponse($data, $message = null, $status=200) {
+        $success = true;
+        return response()->json(compact('success','status','message','data'), $status);
+    }
+
+    protected function sendJsonErrorResponse($data, $message = "Internal Server Error", $status=500) {
+        $success = false;
+        return response()->json(compact('success','status','message'), $status);
+    }
 }
