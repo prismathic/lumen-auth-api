215aea2b66bc54c8ce50f32507a60afcd9a212ac
prismathic <pjjsalam1999@gmail.com>
2020-09-22 09:57:25 +0100

    feat: index page of the API should show lumen version

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ $router->get('/', function () use ($router) {
 });
 
 $router->group(['prefix' => 'api/v1'], function () use ($router) {
+    $router->get('/', function () use ($router) {
+        return $router->app->version();
+    });
     $router->post('register', 'AuthController@register');
     $router->post('login', 'AuthController@login');
 
